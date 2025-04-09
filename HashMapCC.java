020813152ec711f7c46d67f3c2314003d09e2a43
shivakumar <shivamahajan055@gmail.com>
2025-04-09 12:50:25 +0530

    longest Subarray with zero sum

--- HashMapCC.java
+++ HashMapCC.java
@@ -81,9 +81,23 @@ public class HashMapCC{
         }
         System.out.println("Intersection count : "+ count);
     }
+    public static void longestSubarrayWithZeroSum(int arr[]){
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int len = 0;
+        int sum = 0;
+        for(int j=0; j<arr.length; j++){
+            sum += arr[j];
+
+            if(map.containsKey(sum)){
+                len = Math.max(len, j - map.get(sum));
+            }else{
+            map.put(sum, j);
+            }
+        }
+        System.out.println("Longest subarray with zero sum : " + len);
+    }
     public static void main(String[] args){
-       int arr1[] = {7, 3, 9};
-       int arr2[] = {6, 3, 9, 2, 9, 4};
-       unionAndIntersection(arr1, arr2);
+       int arr[] = {15, -2, 2, -8, 1, 7, 10, 23};
+       longestSubarrayWithZeroSum(arr);
     }
 }
